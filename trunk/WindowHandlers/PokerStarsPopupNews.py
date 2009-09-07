26ae5001b446c184896664b75168cac02da523a4
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2009-09-07 08:38:30 +0000

    cleanup: removed superfluous import

--- trunk/WindowHandlers/PokerStarsPopupNews.py
+++ trunk/WindowHandlers/PokerStarsPopupNews.py
@@ -4,8 +4,6 @@ from __future__ import absolute_import
 if __name__ == '__main__':	# see --> http://bugs.python.org/issue1510172 . works only current dir and below
 	import os; __path__ = [os.path.dirname(__file__)]
 
-import re
-
 from . import PokerStarsWindowBase
 
 #********************************************************************************************
