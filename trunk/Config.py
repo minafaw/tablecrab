208a04d1101f909a7747ed89b88492a70e3ec26b
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2009-09-10 22:41:00 +0000

    change: (backwards incompatible) table names for PS tables are now defined in the section header + some new methods in class Config()

--- trunk/Config.py
+++ trunk/Config.py
@@ -93,52 +93,55 @@ class TypeSize(TypePoint): pass
 
 #*************************************************************************************************
 class Config(object):
-	Defaults = {
-				
-			'cli': {
-				'key-pause-keyboard':  TypeKey(None),
-				'key-report-keyboard': TypeKey(None),
-				'key-report-windows': TypeKey(None),
-				'key-info-window': TypeKey(None),
-				'key-info-window-under-mouse': TypeKey(None),
-				},
-				
-			'tables': {
-				'bool-move-mouse-to-active-table': TypeBool(False),
-				},
-			
-			'table': {
-				'key-fold': TypeKey(None),
-				'key-check': TypeKey(None),
-				'key-raise': TypeKey(None),
-				'key-hilight-bet-amount': TypeKey(None),
-				'key-replayer': TypeKey(None),
-				'key-add-one-bb': TypeKey(None),
-				'key-subtract-one-bb': TypeKey(None),
-				'key-add-one-sb': TypeKey(None),
-				'key-subtract-one-sb': TypeKey(None),
-				},
-			
-			'pokerstars': {
-					'bool-close-popup-news': TypeBool(False),
-					},
-			
+	Defaults = (
+			(
+				'cli', (
+					('key-pause-keyboard',  TypeKey(None)),
+					('key-report-keyboard', TypeKey(None)),
+					('key-report-windows', TypeKey(None)),
+					('key-info-window', TypeKey(None)),
+					('key-info-window-under-mouse', TypeKey(None)),
+				),
+			),
+			(
+				'tables', (
+					('bool-move-mouse-to-active-table', TypeBool(False)),
+				),
+			),
+			(
+				'table', (
+					('key-fold', TypeKey(None)),
+					('key-check', TypeKey(None)),
+					('key-raise', TypeKey(None)),
+					('key-hilight-bet-amount', TypeKey(None)),
+					('key-replayer', TypeKey(None)),
+					('key-add-one-bb', TypeKey(None)),
+					('key-subtract-one-bb', TypeKey(None)),
+					('key-add-one-sb', TypeKey(None)),
+					('key-subtract-one-sb', TypeKey(None)),
+				),
+			),
+			(
+				'pokerstars', (
+					('bool-close-popup-news', TypeBool(False)),
+				),
+			),
 			#'pokerstars-tables': [],		# filled in later
-			
-		}
+		)
 		
-	DefaultsPokerStarsTable = {
-			'key': TypeKey(None), 
-			'name': TypeString(''),
-			'size': TypeSize(None),
-			'point-button-check': TypePoint(None),
-			'point-button-fold': TypePoint(None),
-			'point-button-raise': TypePoint(None),
-			'point-checkbox-fold': TypePoint(None),
-			'point-button-replayer-1': TypePoint(None),
-			'point-button-replayer-2': TypePoint(None),
-			}
-	
+	DefaultsPokerStarsTable = (
+			('key', TypeKey(None)), 
+			#'name': TypeString(''),		# filled in later
+			('size', TypeSize(None)),
+			('point-button-check', TypePoint(None)),
+			('point-button-fold', TypePoint(None)),
+			('point-button-raise', TypePoint(None)),
+			('point-checkbox-fold', TypePoint(None)),
+			('point-button-replayer-1', TypePoint(None)),
+			('point-button-replayer-2', TypePoint(None)),
+			)
+			
+		
 	def __init__(self, filePathCfg=FilePathDefaultCfg):
 		
 		self._settings = {}
@@ -156,10 +159,10 @@ class Config(object):
 				userSettings = dict( [(section.lower(), dict(parser.items(section)) ) for section in parser.sections()] )
 			
 		# parse config
-		for (section, options) in self.Defaults.items():
+		for (section, options) in self.Defaults:
 			self._settings[section] = {}
 			userOptions = userSettings.get(section, {})
-			for (option, typeOption) in options.items():
+			for (option, typeOption) in options:
 				userValue = userOptions.get(option, None)
 				if userValue is None:
 					value = typeOption.default
@@ -176,11 +179,12 @@ class Config(object):
 				del userSettings[section]
 				
 		# parse config for pokerStars tables
-		#TODO: on read/write, how to enshure user specified section names remain stable?
 		for (section, userOptions) in userSettings.items():
 			if section.startswith('pokerstars-table-'):
-				table = {}
-				for (option, typeOption) in self.DefaultsPokerStarsTable.items():
+				table = {
+						'name': section[len('pokerstars-table-'): ]
+						}
+				for (option, typeOption) in self.DefaultsPokerStarsTable:
 					userValue = userOptions.get(option, None)
 					if userValue is None:
 						value = typeOption.default
@@ -214,24 +218,35 @@ class Config(object):
 		return self._settings[key]
 	def __setitem__(self, key, value):
 		self._settings[key] = value
-
+		
+	def toConfig(self):
+		result = []
+		for (section, options) in self.Defaults:
+			result.append( ('[%s]' % section).upper() )
+			for option, typeOption in options:
+				value = self._settings[section][option]
+				value = typeOption.toConfig(value)
+				result.append( '%s=%s' % (option, value) )
+			result.append('')
+				
+			# add PS tables following [POKERSTARS] section
+			if section == 'pokerstars':
+				for table in self._settings['pokerstars-tables']:
+					result.append( ('[pokerstars-table-%s]' % table['name']).upper() )
+					for (option, typeOption) in self.DefaultsPokerStarsTable:
+						value = table[option]
+						value = typeOption.toConfig(value)
+						result.append( '%s=%s' % (option, value) )
+					result.append('')
+		return '\n'.join(result)
+		
+	
 #**************************************************************************************************
 if __name__ == '__main__':
 	
 	# shallow test if we our parsing is correct
 	c = Config()
-	for section, value in c._settings.items():
-		if section == 'pokerstars-tables':
-			for i, table in enumerate(value):
-				print '[%s-%s]' % ('pokerstars-table', i)
-				for option, value in table.items():
-					print '%s=%s' % (option, value)
-				print
-		else:
-			print '[%s]' % section
-			for option, value in value.items():
-				print '%s=%s' % (option, value)
-		print
+	print c.toConfig()
 		
 	
 	
