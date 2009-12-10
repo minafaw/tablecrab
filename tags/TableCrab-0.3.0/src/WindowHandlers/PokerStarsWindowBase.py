c0305b23509c103b529798f6af21b6380e3f2964
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2009-12-10 12:37:47 +0000

    release TableCrab-0.3.0

---
+++ tags/TableCrab-0.3.0/src/WindowHandlers/PokerStarsWindowBase.py
@@ -0,0 +1,48 @@
+"""base implementatio for PokerStars windows
+""" 
+from __future__ import absolute_import
+if __name__ == '__main__':	# see --> http://bugs.python.org/issue1510172 . works only current dir and below
+	import os; __path__ = [os.path.dirname(__file__)]
+
+import re
+
+from . import Registry
+
+#********************************************************************************************
+class PokerStarsWindowBase(Registry.WindowHandlerBase):
+	"""base class for PokerStars windows"""
+	
+	Type = None
+	Site = None
+	Window = None
+	
+	PsTitleLobby = 'PokerStars Lobby'
+	PsClassTable = 'PokerStarsTableFrameClass'
+	PsClassNews = '#32770'
+	PsTitleNews = 'News'
+	PsClassInstantHandHistory = '#32770'
+	PsTitleInstantHandHistory = 'Instant Hand History'
+	
+	@classmethod
+	def psIsTable(klass, cli, hWindow):
+		if cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassTable:
+			return True
+		return False
+		
+	@classmethod
+	def psIsPopupNews(klass, cli, hWindow):
+		if cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassNews and \
+							cli.application.windowManager.windowGetText(hWindow) == klass.PsTitleNews:
+			hWindowParent = cli.application.windowManager.windowGetParent(hWindow)
+			if hWindowParent and cli.application.windowManager.windowGetText(hWindowParent).startswith(klass.PsTitleLobby):
+				return True
+		return False
+		
+	@classmethod
+	def psIsInstantHandHistory(klass, cli, hWindow):
+		if cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassInstantHandHistory and \
+					cli.application.windowManager.windowGetText(hWindow) == klass.PsTitleInstantHandHistory:
+			return True
+		return False
+	
+	
---
+++ tags/TableCrab-0.3.0/src/WindowHandlers/__init__.py
@@ -0,0 +1,16 @@
+"""window handlers
+"""
+from __future__ import absolute_import
+if __name__ == '__main__':	# see --> http://bugs.python.org/issue1510172 . works only current dir and below
+	import os; __path__ = [os.path.dirname(__file__)]
+	
+from .Registry import WindowHandlerRegistry
+from .Registry import WindowHandlerType
+
+from . import WindowHandlerDefault
+
+from . import PokerStarsTable
+from . import PokerStarsPopupNews
+from . import PokerStarsInstantHandHistory
+
+
