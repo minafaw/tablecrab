8fd0a4fd9baa06c11ea719010145357dbd385fbf
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2010-07-08 09:27:37 +0000

    refactor a bit to get better detection of PS windows ++ some new methods to determine windows

--- src/WindowHandlers/PokerStarsWindowBase.py
+++ src/WindowHandlers/PokerStarsWindowBase.py
@@ -17,32 +17,72 @@ class PokerStarsWindowBase(Registry.WindowHandlerBase):
 	Window = None
 	
 	PsTitleLobby = 'PokerStars Lobby'
+	PsClassLobby = '#32770'	# duh, stars. main windows should never be dialogs
 	PsClassTable = 'PokerStarsTableFrameClass'
 	PsClassNews = '#32770'
 	PsTitleNews = 'News'
 	PsClassInstantHandHistory = '#32770'
 	PsTitleInstantHandHistory = 'Instant Hand History'
+	PsTitleTourneyRegistrationMessageBox = 'Tournament Registration'
+	PsClassTourneyRegistrationMessageBox = '#32770'
+	PsTitleTableMessageBox = 'PokerStars'
+	PsClassTableMessageBox = '#32770'
+	
+	
+	#NOTE: not easy to determine if a window is a PokerStarswindow. i don't feel like messing around with psapi.dll to get the exefilename.
+	# so ..i use a hack. we identify a window as being a stars window if it (or one of its parents) contains a widget classname.startswith('PokerStars')
+	# nasty and errorprone, but the most reasonalbe i could come up with now
+	@classmethod
+	def _psHasPokerStarsWidgets(klass, cli, hWindow):
+		for hWindow in cli.application.windowManager.windowChildren(hWindow):
+			if cli.application.windowManager.windowGetClassName(hWindow).startswith('PokerStars'):	return True
+		return False
+	@classmethod
+	def psIsPokerStarsWindow(klass, cli, hWindow):
+		while hWindow:
+			if klass._psHasPokerStarsWidgets(cli, hWindow): return True
+			hWindow = cli.application.windowManager.windowGetParent(hWindow)
+		return False	
 	
 	@classmethod
-	def psIsTable(klass, cli, hWindow):
-		if cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassTable:
-			return True
+	def psIsLobby(klass, cli, hWindow):
+		if not cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassLobby: return False
+		if not cli.application.windowManager.windowGetText(hWindow).startswith(klass.PsTitleLobby): return False
+		if not klass.psIsPokerStarsWindow(cli, hWindow): return False
 		return False
+		
+	@classmethod
+	def psIsTable(klass, cli, hWindow):
+		if not cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassTable: return False
+		return True
 		
 	@classmethod
 	def psIsPopupNews(klass, cli, hWindow):
-		if cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassNews and \
-							cli.application.windowManager.windowGetText(hWindow) == klass.PsTitleNews:
-			hWindowParent = cli.application.windowManager.windowGetParent(hWindow)
-			if hWindowParent and cli.application.windowManager.windowGetText(hWindowParent).startswith(klass.PsTitleLobby):
-				return True
-		return False
+		if not cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassNews: return False
+		if not cli.application.windowManager.windowGetText(hWindow) == klass.PsTitleNews: return False
+		if not klass.psIsPokerStarsWindow(cli, hWindow): return False
+		return True
 		
 	@classmethod
 	def psIsInstantHandHistory(klass, cli, hWindow):
-		if cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassInstantHandHistory and \
-					cli.application.windowManager.windowGetText(hWindow) == klass.PsTitleInstantHandHistory:
-			return True
-		return False
+		if not cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassInstantHandHistory: return False
+		if not cli.application.windowManager.windowGetText(hWindow) == klass.PsTitleInstantHandHistory: return False
+		if not klass.psIsPokerStarsWindow(cli, hWindow): return False
+		return True
+
+	@classmethod
+	def psIsTourneyRegistrationMessageBox(klass, cli, hWindow):
+		if not cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassTourneyRegistrationMessageBox: return False
+		if not cli.application.windowManager.windowGetText(hWindow) == klass.PsTitleTourneyRegistrationMessageBox: return False
+		if not klass.psIsPokerStarsWindow(cli, hWindow): return False
+		return True
 	
-	
+	@classmethod
+	def psIsTableMessageBox(klass, cli, hWindow):
+		if not cli.application.windowManager.windowGetClassName(hWindow) == klass.PsClassTableMessageBox: return False
+		if not cli.application.windowManager.windowGetText(hWindow) == klass.PsTitleTableMessageBox: return False
+		hWindowParent = cli.application.windowManager.windowGetParent(hWindow)
+		if not klass.psIsTable(hWindowParent): return False
+		return True
+
+
