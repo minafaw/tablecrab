47ffd9b84b600f6d04e048c85a90b0a0a1fd7bb3
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2010-07-28 21:02:33 +0000

    adapt to recent changes

---
+++ branches/TableCrab2/TableCrabRes/StyleSheets.py
@@ -0,0 +1,9 @@
+# auto generated resource file. WARNING: do not edit
+from PyQt4 import QtCore
+_cache = {}
+def default():
+	arr = _cache.get("default", None)
+	if arr is None:
+		arr = QtCore.QByteArray.fromBase64("CmJvZHl7YmFja2dyb3VuZC1jb2xvcjogZ3JheTt9")
+		_cache["default"] = arr
+	return QtCore.QString(arr)
