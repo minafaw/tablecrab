ff7eb0b40c7c1e5e312c74141c57588f013677d6
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2010-08-10 08:56:43 +0000

    todo

--- branches/TableCrab2/TableCrabGuiSetup.py
+++ branches/TableCrab2/TableCrabGuiSetup.py
@@ -1,8 +1,8 @@
  
 #TODO: in wine we-1.2 can not resize windows belonging to other processes. this a bug in wine [ http://bugs.winehq.org/show_bug.cgi?id=23940 ] 
-#				user32.SetWindowPos()	does not work for other processes. used to work in wine-1.14, stopped working in wine-1.2. as soon as SetWindowPos() 
-#				is working again we should kick out mouse tracking on other windows and add a hotkey to resize the current window to the size of the currently 
-#				selected tempplate
+#				user32.SetWindowPos() does not work for other processes. used to work in wine-1.1.41, stopped working in wine-1.1.42. as soon as SetWindowPos() 
+#				is working again we should kick out mouse tracking on other windows and add a hotkey to resize the current window to match the size of a tempplate.
+#				alternatively we could add a hotkey directly to to a template.
 #TODO: i kind of dislike in-place editing of template names. an "Edit" button would be more consistent but a bit of overkill right now. then .again .screenshot 
 # 			open / save is taking away screenspace already. would have to find shorter names for these actions.
 #TODO: when the mouse leaves screenshot would be nice to set coordianteddisplayed to (-1, -1) or "None"
