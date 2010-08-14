ddf53f828942ecbb89920d972ac6dfd06f118373
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2010-08-14 11:18:44 +0000

    foo

---
+++ TableCrab-0.1.0/src/WindowHandlers/Registry.py
@@ -0,0 +1,47 @@
+ 
+WindowHandlerRegistry = {}
+WindowHandlerType = 'WindowHandler'
+
+class WindowHandlerMeta(type):
+	"""metaclass for all window handlers
+	"""
+	def __new__(klass, name, bases, kws):
+		newKlass = type.__new__(klass, name, bases, kws)
+		if newKlass.Type is None:
+			return newKlass
+		typeKlass = newKlass.Type
+		if newKlass.Site is None and newKlass.Window is not None:
+			raise ValueError('if windowHandlers define Site, Window must be defined aswell')
+		if newKlass.Site is not None:
+			typeKlass += ':%s' % newKlass.Site
+			if newKlass.Window is not None:
+				typeKlass += ':%s' % newKlass.Window
+		if typeKlass in WindowHandlerRegistry:
+			raise ValueError('windowHandler already registered: %s' % typeKlass)
+		newKlass.Type = typeKlass
+		WindowHandlerRegistry[typeKlass] = newKlass
+		return newKlass
+
+
+class WindowHandlerBase(object):
+	__metaclass__ = WindowHandlerMeta
+	Type = None			# should always be WindowHandlerType. if None, the class is not registered as WindowHandler
+	Site = None			# site handled
+	Window = None		# window handled
+	
+	@classmethod
+	def handleWindowCreated(klass, cli, hWindow):
+		return None
+		
+	def handleWindowDestroyed(self, cli, hWindow):
+		return False
+	def handleWindowGainForeground(self, cli, hWindow):
+		return False
+	def handleWindowLooseForeground(self, cli, hWindow):
+		return False
+	def handleKeyPressed(self, cli, key):
+		return False
+	def handleKeyReleased(self, cli, key):
+		return False
+	
+	
