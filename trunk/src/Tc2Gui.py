c3e6dd4940935a1e044993c9d8b3629290e55b7d
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2010-11-21 00:54:29 +0000

    fix: conflicting shortcut for "open" action and tools tab. tools tab is now "Alt+L"

--- trunk/src/Tc2Gui.py
+++ trunk/src/Tc2Gui.py
@@ -86,7 +86,7 @@ class Gui(QtGui.QMainWindow):
 		self.tabSetup = self._addTab(Tc2GuiSetup.FrameSetup, 'Se&tup')
 		self.tabHotkeys = self._addTab(Tc2GuiHotkeys.FrameHotkeys, 'Hot&keys')
 		self.tabHand = self._addTab(Tc2GuiHandViewer.FrameHandViewer, 'H&and')
-		self.tabTools = self._addTab(Tc2GuiTools.FrameTools, 'T&ools')
+		self.tabTools = self._addTab(Tc2GuiTools.FrameTools, 'Too&ls')
 		self.tabSettings = self._addTab(Tc2GuiSettings.FrameSettings, 'Settin&gs')
 		self.tabHelp = self._addTab(Tc2GuiHelp.FrameHelp, '&Help')
 		self.tabWidget.currentChanged.connect(self.onTabCurrentChanged)
