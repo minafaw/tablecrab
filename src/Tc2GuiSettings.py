2a8e53dc85e6da6376de70ac86069491924e1f83
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2010-11-24 11:56:24 +0000

    new feature: (experimental!) fetch nash calculations from HoldemResources while browsing hand histories

--- src/Tc2GuiSettings.py
+++ src/Tc2GuiSettings.py
@@ -9,6 +9,7 @@ import Tc2GuiSettingsGlobal
 import Tc2GuiSettingsPokerStars
 import Tc2GuiSettingsHandViewer
 import Tc2GuiSettingsHandViewerStyleSheet
+import Tc2GuiSettingsNashCalculationsStyleSheet
 
 from PyQt4 import QtCore, QtGui
 
@@ -39,6 +40,7 @@ class FrameSettings(QtGui.QFrame):
 		self.settingsPokerStars = self.addSetting('PokerStars', Tc2GuiSettingsPokerStars.FrameSettings(parent=self.stack), 'Shift+P', 'PokerStars (Shift+P)')
 		self.settingsHand = self.addSetting('Hand viewer', Tc2GuiSettingsHandViewer.FrameSettings(parent=self.stack), 'Shift+H', 'Hand viewer (Shift+H)')
 		self.settingsHandStyleSheet = self.addSetting('Hand viewer style sheet', Tc2GuiSettingsHandViewerStyleSheet.FrameSettings(parent=self.stack), 'Shift+S', 'Hand viewer style sheet (Shift+S)')
+		self.settingsNashCalculationsStyleSheet = self.addSetting('Nash calculations style sheet', Tc2GuiSettingsNashCalculationsStyleSheet.FrameSettings(parent=self.stack), 'Shift+S', 'Nash calculations style sheet (Shift+N)')
 
 		Tc2Config.globalObject.init.connect(self.onInit)
 		Tc2Config.globalObject.closeEvent.connect(self.onCloseEvent)
--- src/Tc2GuiToolsHandHistoryViewer.py
+++ src/Tc2GuiToolsHandHistoryViewer.py
@@ -2,19 +2,136 @@
 import Tc2Config
 import Tc2HandGrabberPokerStars
 import Tc2GuiHelp
+from Tc2Lib import HoldemResources
 from PyQt4 import QtCore, QtGui, QtWebKit
 import codecs
+#************************************************************************************
+#
+#************************************************************************************
+class FrameNashCalculations(QtGui.QFrame):
+
+	SettingsKeyBase = 'Gui/Tools/PHandHistoryViewer'
+	SettingsKeyStyleSheet = SettingsKeyBase + 'NashCalculations/StyleSheet'
+	PayoutStructures = (
+			('None', ''),
+			('PokerStars 9 man sitNgo', '50, 30, 20'),
+			('PokerStars 6 man sitNgo', '65, 35'),
+			)
+
+	def __init__(self, parent, toolBar=None):
+		QtGui.QFrame.__init__(self, parent)
+
+		self.lastHand = None
+
+		parent.handSet.connect(self.onHandSet)
+		if toolBar is not None:
+			toolBar.zoomFactorChanged.connect(self.onZoomFactorChanged)
+
+		self.fetcher = HoldemResources.NashFetcher()
+		self.formatter = HoldemResources.NashFormatter()
+
+		self.label = QtGui.QLabel('Select payouts to fetch nash calculations for hand', self)
+		self.edit = QtGui.QLineEdit(self)
+		self.edit.setEnabled(False)
+		self.edit.setInputMask('99,99,99,99,99,99,99,99,99,99')
+		self.webView = QtWebKit.QWebView(self)
+
+		self.comboBox = QtGui.QComboBox(self)
+		for i, (text, _) in enumerate(self.PayoutStructures):
+			self.comboBox.addItem(text, i)
+
+		# connect signals
+		Tc2Config.globalObject.init.connect(self.onInit)
+
+	def payoutStructure(self):
+		return [round( int(i) / 100.0, 2) for i in str(self.edit.text()).split(',') if i]
+
+	def layout(self):
+		grid = Tc2Config.GridBox(self)
+		grid.setRowStretch(0, 99)
+		grid.col(self.webView, colspan=2)
+		grid.row()
+		grid.col(self.label, colspan=2)
+		grid.row()
+		grid.col(self.comboBox)
+		grid.col(self.edit)
+
+
+	def setHand(self, hand):
+		self.lastHand = hand
+		if hand is None:
+			self.webView.setHtml('')
+			return
+		payoutStructure = self.payoutStructure()
+		if not payoutStructure:
+			self.webView.setHtml('')
+			return
+		seats = hand.seatsButtonOrdered()
+		if len(seats) == 1:
+			return
+		elif len(seats) > 3:
+			seats.append(seats.pop(0))
+			seats.append(seats.pop(0))
+			seats.append(seats.pop(0))
+		stacks = [seat.stack for seat in seats]
+		try:
+			url, data = self.fetcher.getData(
+					bigBlind=hand.blindBig,
+					smallBlind=hand.blindSmall,
+					ante=hand.blindAnte,
+					payouts=payoutStructure,
+					stacks=stacks,
+					proxy=None,
+					)
+		except HoldemResources.FetchError, details:
+			Tc2Config.msgCritical('could not fetch data:%s\n%s' % (details) )
+			return
+		self.formatter.parse(data)
+
+		def sortf(seats, mySeats=hand.seats, mySeatsButtonOrder=seats):
+			mySeats = [i for i in mySeats if i is not None]
+			result = [None]* len(mySeatsButtonOrder)
+			for i, seat in enumerate(seats):
+				mySeat = mySeatsButtonOrder[i]
+				myIndex = mySeats.index(mySeat)
+				result[myIndex] = seat
+			return result
+
+		html = self.formatter.toHtml(
+				seatSortf=sortf,
+				styleSheet=Tc2Config.settingsValue(self.SettingsKeyStyleSheet, HoldemResources.NashFormatter.StyleSheet).toString()
+				)
+		self.webView.setHtml(html)
+
+	def onInit(self):
+		self.layout()
+		self.comboBox.currentIndexChanged.connect(self.onComboBoxCurrentIndexChanged)
+		self.webView.setHtml('')
+
+	def onComboBoxCurrentIndexChanged(self, i):
+		self.edit.setText(self.PayoutStructures[i][1])
+		self.edit.setEnabled(i != 0)
+		self.setHand(self.lastHand)
+
+	def onHandSet(self, hand):
+		self.setHand(hand)
+
+	def onZoomFactorChanged(self, factor):
+		self.webView.setZoomFactor(factor)
 
 #************************************************************************************
 #
 #************************************************************************************
 class FrameTool(QtGui.QFrame):
 
+	handSet = QtCore.pyqtSignal(QtCore.QObject)
+
 	SettingsKeyBase = 'Gui/Tools/PHandHistoryViewer'
 	SettingsKeyZoomFactor = SettingsKeyBase + '/ZoomFactor'
 	SettingsKeyDialogOpenState = SettingsKeyBase + '/DialogOpen/State'
 	SettingsKeyDialogSaveState = SettingsKeyBase + '/DialogSave/State'
 	SettingsKeyDialogSaveHandState = SettingsKeyBase + '/DialogSaveHand/State'
+	SettingsKeySplitterState = SettingsKeyBase + '/SplitterState'
 
 	def __init__(self, parent=None):
 		QtGui.QFrame.__init__(self, parent)
@@ -30,7 +147,11 @@ class FrameTool(QtGui.QFrame):
 		self.spinBox.setRange(0, 0)
 		self.spinBox.setSuffix(' /0')
 
+
+		self.splitter = QtGui.QSplitter(QtCore.Qt.Horizontal, self)
+
 		self.webView = QtWebKit.QWebView(self)
+		self.splitter.addWidget(self.webView)
 		self.webView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
 		self.webView.customContextMenuRequested.connect(self.onContextMenuWebView)
 
@@ -43,6 +164,9 @@ class FrameTool(QtGui.QFrame):
 		self.toolBar = Tc2Config.WebViewToolBar(self.webView,
 				settingsKeyZoomFactor=self.SettingsKeyZoomFactor
 				)
+
+		self.frameNashCalculations = FrameNashCalculations(self, toolBar=self.toolBar)
+		self.splitter.addWidget(self.frameNashCalculations)
 
 		# set up actions
 		self.actionCopy = self.webView.pageAction(QtWebKit.QWebPage.Copy)
@@ -83,6 +207,7 @@ class FrameTool(QtGui.QFrame):
 
 		# connect signals
 		Tc2Config.globalObject.init.connect(self.onInit)
+		Tc2Config.globalObject.closeEvent.connect(self.onCloseEvent)
 		self.webView.urlChanged.connect(self.onUrlChanged)
 		Tc2Config.globalObject.settingToolBarPositionChanged.connect(self.onSettingToolBarPositionChanged)
 
@@ -94,7 +219,7 @@ class FrameTool(QtGui.QFrame):
 			grid.row()
 		grid.col(self.spinBox)
 		grid.row()
-		grid.col(self.webView)
+		grid.col(self.splitter)
 		if toolBarPositionBottom:
 			grid.row()
 			grid.col(self.toolBar)
@@ -109,6 +234,7 @@ class FrameTool(QtGui.QFrame):
 		self.layout()
 		self.webView.setUrl(QtCore.QUrl(''))
 		self.spinBox.valueChanged.connect(self.onSpinBoxValueChanged)
+		self.splitter.restoreState( Tc2Config.settingsValue(self.SettingsKeySplitterState, QtCore.QByteArray()).toByteArray() )
 		self.adjustActions()
 
 	def adjustActions(self):
@@ -144,6 +270,9 @@ class FrameTool(QtGui.QFrame):
 			self.spinBox.setRange(0, 0)
 			self.spinBox.setSuffix(' /0' )
 		self.adjustActions()
+
+	def onCloseEvent(self, event):
+		Tc2Config.settingsSetValue(self.SettingsKeySplitterState, self.splitter.saveState())
 
 	def onActionSaveTriggered(self):
 		fileName = Tc2Config.dlgOpenSaveFile(
@@ -204,9 +333,12 @@ class FrameTool(QtGui.QFrame):
 					hand = self.handParser.parse(data)
 				except:
 					Tc2Config.handleException('\n' + data)
+					#TODO: data = ?
 				else:
 					data = self.handFormatter.dump(hand)
+					self.handSet.emit(hand)
 				networkReply.setData(data, 'text/html; charset=UTF-8')
+
 
 	def onSpinBoxValueChanged(self, handNo):
 		self.webView.setUrl(QtCore.QUrl('hand:///%s' % handNo))
