eb9a3ad7c75d531a935dc265fb094ecf8c0a4958
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2010-11-28 19:46:34 +0000

    add setting random mode to "shot clock"

--- src/Tc2Config.py
+++ src/Tc2Config.py
@@ -89,7 +89,7 @@ def handleException(data=None):
 #************************************************************************************
 #
 #************************************************************************************
-import posixpath, thread, inspect
+import posixpath, thread, inspect, random
 from PyQt4 import QtCore, QtGui, QtWebKit, QtNetwork
 
 import Tc2Win32
@@ -508,10 +508,11 @@ class ClockLabel(QtGui.QLabel):
 	PrecissionMax = 5
 	IncrementMin = 1
 	IncrementMax = 100
-	def __init__(self, parent=None, precission=PrecissionMin, increment=IncrementMin, speed=SpeedMin):
+	def __init__(self, parent=None, precission=PrecissionMin, increment=IncrementMin, speed=SpeedMin, randomMode=False):
 		QtGui.QLabel.__init__(self, parent)
 		self._precission = precission
 		self. _increment = increment
+		self._randomMode = randomMode
 		self._value = 0
 		self.setText(str(self._value).zfill(self._precission))
 		self._timer = QtCore.QTimer(self)
@@ -528,6 +529,8 @@ class ClockLabel(QtGui.QLabel):
 		settings.precissionChanged.connect(self.setPrecission)
 		self.setSpeed(settings.speed())
 		settings.speedChanged.connect(self.setSpeed)
+		self.setRandomMode(settings.randomMode())
+		settings.randomModeChanged.connect(self.setRandomMode)
 	def setIncrement(self, value):
 		wasOn = self.setOn(False)
 		self._increment = value
@@ -540,6 +543,8 @@ class ClockLabel(QtGui.QLabel):
 		wasOn = self.setOn(False)
 		self._timer.setInterval(value * 1000)
 		if wasOn: self.setOn(True)
+	def setRandomMode(self, flag):
+		self._randomMode = flag
 	def isOn(self):
 		return self._timer.isActive()
 	def setOn(self, flag):
@@ -547,10 +552,17 @@ class ClockLabel(QtGui.QLabel):
 		self._timer.start() if flag else self._timer.stop()
 		return wasOn
 	def tick(self):
-		self._value += self._increment
-		if self._value >= 10**self._precission:
-			self._value= 0
+		maxValue = 10**self._precission
+		if self._randomMode:
+			value = random.randint(1, maxValue)
+			f = float(value) / self._increment
+			self._value = int(self._increment * round(f, 0) )
+		else:
+			self._value += self._increment
+		if self._value >= maxValue:
+			self._value = 0
 		self.setText(str(self._value).zfill(self._precission))
+
 
 class WebViewToolBar(QtGui.QToolBar):
 	ZoomMin = 0.5
