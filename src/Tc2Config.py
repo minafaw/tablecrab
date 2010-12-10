460b65994796d8bc9f14af83162235106e099d3f
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2010-12-10 09:35:48 +0000

    prep release

--- src/Tc2Config.py
+++ src/Tc2Config.py
@@ -6,7 +6,7 @@
 # project consts
 #************************************************************************************
 ApplicationName = 'TableCrab2'
-Version = '0.6.2'
+Version = '0.6.3'
 ReleaseName = '%s-%s' % (ApplicationName, Version)
 Author = 'JuergenUrner'
 ErrorLogName = ApplicationName + '-Error.log'
