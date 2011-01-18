a984662ff56271d63e8eab399ca7722ee406ff7b
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2011-01-18 06:31:34 +0000

    GuiSettings: clean up

--- trunk/src/Tc2GuiSettings.py
+++ trunk/src/Tc2GuiSettings.py
@@ -29,39 +29,39 @@ class FrameSettings(QtGui.QFrame):
 	def __init__(self, parent=None):
 		QtGui.QFrame.__init__(self, parent)
 
-		self.listWidget = QtGui.QListWidget(self)
-		self.listWidget.itemSelectionChanged.connect(self.onSettingSelected)
+		self._listWidget = QtGui.QListWidget(self)
+		self._listWidget.itemSelectionChanged.connect(self.onSettingSelected)
 
-		self.stack = QtGui.QStackedWidget(self)
+		self._stack = QtGui.QStackedWidget(self)
 
-		self.splitter = QtGui.QSplitter(self)
-		self.splitter.addWidget(self.listWidget)
-		self.splitter.addWidget(self.stack)
+		self._splitter = QtGui.QSplitter(self)
+		self._splitter.addWidget(self._listWidget)
+		self._splitter.addWidget(self._stack)
 
 		#
-		self.settingsGlobal = self.addSetting('Global', Tc2GuiSettingsGlobal.FrameSettings(parent=self.stack), 'Shift+G', 'Global (Shift+G)')
-		self.settingsNetwork = self.addSetting('Network', Tc2GuiSettingsNetwork.FrameSettings(parent=self.stack), 'Shift+G', 'Global (Shift+N)')
-		self.settingsPokerStars = self.addSetting('PokerStars', Tc2GuiSettingsPokerStars.FrameSettings(parent=self.stack), 'Shift+P', 'PokerStars (Shift+P)')
-		self.settingsHand = self.addSetting('HandViewer', Tc2GuiSettingsHandViewer.FrameSettings(parent=self.stack), 'Shift+H', 'Hand viewer (Shift+H)')
-		self.settingsHandStyleSheet = self.addSetting('HandViewerStyleSheet', Tc2GuiSettingsHandViewerStyleSheet.FrameSettings(parent=self.stack), 'Shift+S', 'Hand viewer style sheet (Shift+S)')
-		self.settingsNashCalculationsStyleSheet = self.addSetting('NashCalculationsStyleSheet', Tc2GuiSettingsNashCalculationsStyleSheet.FrameSettings(parent=self.stack), 'Shift+S', 'Nash calculations style sheet (Shift+C)')
-		self.settingsCardProtector = self.addSetting('CardProtector', Tc2GuiSettingsCardProtector.FrameSettings(parent=self.stack), 'Shift+A', 'Card Protector (Shift+A)')
-		self.settingsClock = self.addSetting('Clock', Tc2GuiSettingsClock.FrameSettings(parent=self.stack), 'Shift+C', 'Clock (Shift+C)')
+		self.settingsGlobal = self.addSetting('Global', Tc2GuiSettingsGlobal.FrameSettings(parent=self._stack), 'Shift+G', 'Global (Shift+G)')
+		self.settingsNetwork = self.addSetting('Network', Tc2GuiSettingsNetwork.FrameSettings(parent=self._stack), 'Shift+G', 'Global (Shift+N)')
+		self.settingsPokerStars = self.addSetting('PokerStars', Tc2GuiSettingsPokerStars.FrameSettings(parent=self._stack), 'Shift+P', 'PokerStars (Shift+P)')
+		self.settingsHand = self.addSetting('HandViewer', Tc2GuiSettingsHandViewer.FrameSettings(parent=self._stack), 'Shift+H', 'Hand viewer (Shift+H)')
+		self.settingsHandStyleSheet = self.addSetting('HandViewerStyleSheet', Tc2GuiSettingsHandViewerStyleSheet.FrameSettings(parent=self._stack), 'Shift+S', 'Hand viewer style sheet (Shift+S)')
+		self.settingsNashCalculationsStyleSheet = self.addSetting('NashCalculationsStyleSheet', Tc2GuiSettingsNashCalculationsStyleSheet.FrameSettings(parent=self._stack), 'Shift+S', 'Nash calculations style sheet (Shift+C)')
+		self.settingsCardProtector = self.addSetting('CardProtector', Tc2GuiSettingsCardProtector.FrameSettings(parent=self._stack), 'Shift+A', 'Card Protector (Shift+A)')
+		self.settingsClock = self.addSetting('Clock', Tc2GuiSettingsClock.FrameSettings(parent=self._stack), 'Shift+C', 'Clock (Shift+C)')
 
 		Tc2Config.globalObject.initSettingsFinished.connect(self.onGlobalObjectInitSettingsFinished)
 		Tc2Config.globalObject.closeEvent.connect(self.onCloseEvent)
 
 	def layout(self):
 		grid = Tc2Config.GridBox(self)
-		grid.col(self.splitter)
+		grid.col(self._splitter)
 
 	def addSetting(self, name, widget, shortcut, toolTip):
-		item = QtGui.QListWidgetItem(name, self.listWidget)
+		item = QtGui.QListWidgetItem(name, self._listWidget)
 		item.setToolTip(toolTip)
-		self.listWidget.addItem(item)
-		self.stack.addWidget(widget)
+		self._listWidget.addItem(item)
+		self._stack.addWidget(widget)
 		action = QtGui.QAction(self)
-		action.setData(QtCore.QVariant(self.listWidget.count() -1) )
+		action.setData(QtCore.QVariant(self._listWidget.count() -1) )
 		action.setShortcut(shortcut)
 		action.triggered.connect(self.onSettingsShortcut)
 		self.addAction(action)
@@ -69,26 +69,26 @@ class FrameSettings(QtGui.QFrame):
 
 	def onSettingsShortcut(self):
 		index = self.sender().data().toInt()[0]
-		self.listWidget.setCurrentRow(index)
+		self._listWidget.setCurrentRow(index)
 		self.onSettingSelected()
 
 	def onSettingSelected(self):
-		row = self.listWidget.currentRow()
+		row = self._listWidget.currentRow()
 		if row < 0:
 			row = 0
-		self.stack.setCurrentIndex(row)
+		self._stack.setCurrentIndex(row)
 
 	def onCloseEvent(self, event):
-		Tc2Config.settingsSetValue(self.SettingsKeySplitterState, self.splitter.saveState())
-		Tc2Config.settingsSetValue(self.SettingsKeyCurrentSettingIndex, self.stack.currentIndex())
+		Tc2Config.settingsSetValue(self.SettingsKeySplitterState, self._splitter.saveState())
+		Tc2Config.settingsSetValue(self.SettingsKeyCurrentSettingIndex, self._stack.currentIndex())
 
 	def onGlobalObjectInitSettingsFinished(self, globalObject):
 		self.layout()
-		self.splitter.restoreState( Tc2Config.settingsValue(self.SettingsKeySplitterState, QtCore.QByteArray()).toByteArray() )
-		self.listWidget.setCurrentRow( Tc2Config.settingsValue(self.SettingsKeyCurrentSettingIndex, 0).toInt()[0] )
+		self._splitter.restoreState( Tc2Config.settingsValue(self.SettingsKeySplitterState, QtCore.QByteArray()).toByteArray() )
+		self._listWidget.setCurrentRow( Tc2Config.settingsValue(self.SettingsKeyCurrentSettingIndex, 0).toInt()[0] )
 
-		self.listWidget.setAlternatingRowColors(globalObject.settingsGlobal.alternatingRowColors())
-		globalObject.settingsGlobal.alternatingRowColorsChanged.connect(self.listWidget.setAlternatingRowColors)
+		self._listWidget.setAlternatingRowColors(globalObject.settingsGlobal.alternatingRowColors())
+		globalObject.settingsGlobal.alternatingRowColorsChanged.connect(self._listWidget.setAlternatingRowColors)
 
 
 
