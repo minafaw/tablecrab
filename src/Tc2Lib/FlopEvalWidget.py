52832af4a8900aa0c1147b46b163435a1195c7c6
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2011-06-18 11:42:36 +0000

    FlopEvalWidget: handle invalid user input

--- src/Tc2Lib/FlopEvalWidget.py
+++ src/Tc2Lib/FlopEvalWidget.py
@@ -1,5 +1,8 @@
 """"""
 
+#TODO: how to give feedback when user enters an invalid hand range? current approach
+#      of flashing handRangeButtons is a bit well ..weird.
+
 from PyQt4 import QtCore, QtGui
 import itertools
 import PokerTools
@@ -31,14 +34,42 @@ class FloatProgressBar(QtGui.QProgressBar):
 class FlopEvalWidget(QtGui.QFrame):
 	
 	
+	class ErrTimer(QtCore.QTimer):
+		def __init__(self, parent):
+			QtCore.QTimer.__init__(self, parent)
+			self.setSingleShot(False)
+			self.timeout.connect(self.onTimeout)
+			self.handRange = None
+			self.handRangeText = None
+			self.n = 0
+		def start(self):
+			self.handRange = self.parent().handRangeWidget.handRangeFromHandTypes()
+			self.handRangeText = self.parent().handRangeWidget.handRangeText()
+			QtCore.QTimer.start(self, 100)
+		def onTimeout(self):
+			self.n += 1
+			if self.n == 1:
+				self.parent().handRangeWidget.setHandRange(
+				PokerTools.HandRangeHoldem.fromString(
+						'K4o,K3o,Q4o, T4o,T3o,94o, 74o,73o,64o'
+						))
+			elif self.n == 2:
+				self.parent().handRangeWidget.setHandRange(self.handRange)
+				self.parent().handRangeWidget.setHandRangeText(self.handRangeText)
+				self.n = 0
+				self.stop()
+		
+	
 	def __init__(self, parent=None, styleSheet=None):
 		QtGui.QFrame.__init__(self, parent)
 		
 		self.handEval = TexasHoldem.HandEval()
 		self.flopEval = FlopEval.FlopEval()
+		self.errTimer = self.ErrTimer(self)
 		
 		self.labelHandRange = QtGui.QLabel('Select a hand range', self)
 		self.handRangeWidget = HandRangeWidget.HandRangeWidget(parent=self, pct=15)
+		self.handRangeWidget.invalidHandRangeEntered.connect(self.onInvalidHandRangeEntered)
 		self.labelFlop = QtGui.QLabel('Select a flop', self)
 		self.labelFlop.setWordWrap(True)
 		self.flopWidget = CardSelectorWidget.CardSelectorWidget(parent=self, maxCards=3)
@@ -230,6 +261,8 @@ class FlopEvalWidget(QtGui.QFrame):
 		self._resetResult()
 		flop = self.flopWidget.cards()
 		handRange = self.handRangeWidget.handRange()
+		if handRange is None:
+			return
 		result = self._makeResult()
 		for hand in handRange:
 			# skip hands with cards on flop
@@ -241,12 +274,17 @@ class FlopEvalWidget(QtGui.QFrame):
 	def onButtonEvalRandomClicked(self):
 		self._resetResult()
 		handRange = self.handRangeWidget.handRange()
+		if handRange is None:
+			return
 		result = self._makeResult()
 		result.update(self.flopEval.evalHandRange(handRange))
 		self._setResult(result)
 			
 	def onflopWidgetCardsSelectionChanged(self, widget):
 		self.buttonEvalFlop.setEnabled(widget.cardCount() == 3)
+		
+	def onInvalidHandRangeEntered(self, handRangeWidget):
+		self.errTimer.start()
 
 #************************************************************************************
 #
