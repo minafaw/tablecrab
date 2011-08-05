8b1013a579b858e1284a08aa597f387a37e64796
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2011-08-05 09:58:35 +0000

    tag release

---
+++ TableCrab2-0.7.4/src/Tc2Res/StyleSheets.py
@@ -0,0 +1,9 @@
+# auto generated resource file. WARNING: do not edit
+from PyQt4 import QtCore
+_cache = {}
+def default():
+	arr = _cache.get("default", None)
+	if arr is None:
+		arr = QtCore.QByteArray.fromBase64("CmJvZHl7Cglib3JkZXI6IHNvbGlkIGJsYWNrIDFweDsKCW1hcmdpbjogMGVtIDBlbSAwZW0gMGVtOwoJcGFkZGluZzogMC41ZW07CgltYXJnaW4tYm90dG9tOiAxMGVtOwoJfQoKLmhlYWRlckJveHsKCWJhY2tncm91bmQtY29sb3I6ICNDRURGRjI7Cglib3JkZXI6IHNvbGlkIDFweDsKCW1hcmdpbi1ib3R0b206IDFlbTsKCWZvbnQtd2VpZ2h0OiBib2xkOwoJcGFkZGluZzogMC4yZW07Cgl9Ci50ZXh0Qm94ewoJYmFja2dyb3VuZC1jb2xvcjogI0ZBRkFGQTsKCWJvcmRlcjogc29saWQgYmxhY2sgMXB4OwoJcGFkZGluZzogMC41ZW07Cgl9Ci5ub3RlQm94ewoJYm9yZGVyOiBzb2xpZCBibGFjayAxcHg7CgliYWNrZ3JvdW5kLWNvbG9yOiAjRjhFNUE0OwoJbWFyZ2luOiAxZW0gMmVtIDFlbSAwZW07CglwYWRkaW5nOiAwLjJlbTsKCX0=")
+		_cache["default"] = arr
+	return QtCore.QString(arr)
