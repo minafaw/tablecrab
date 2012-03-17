6ac5c6764aee2ea4bd36c60cabb1944473ebdaef
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2012-03-17 08:18:56 +0000

    increment version number

--- src/Tc2Config.py
+++ src/Tc2Config.py
@@ -6,7 +6,7 @@
 # project consts
 #************************************************************************************
 ApplicationName = 'TableCrab2'
-Version = '0.7.8'
+Version = '0.7.9'
 ReleaseName = '%s-%s' % (ApplicationName, Version)
 Author = 'JuergenUrner'
 ErrorLogName = ApplicationName + '-Error.log'
