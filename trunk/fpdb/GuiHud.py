954971a67335a9ed279da8483217f8ab8737dfb1
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2012-04-29 16:20:38 +0000

    #fpdb# added a basic proove of concept implementation of a HUD running on platform dependend window managers

---
+++ trunk/fpdb/GuiHud.py
@@ -0,0 +1,77 @@
+
+import pygtk
+import gtk
+import gobject
+
+import Platform
+
+#************************************************************************************
+#
+#************************************************************************************
+class Hud(gtk.Window):
+	def __init__(self, platformWindow):
+		gtk.Window.__init__(self)
+		self.set_title('Hud')
+
+		# init hud for window
+		#NOTE: have to realize() window first, otherwise window has no handle
+		#NOTE: have to set stransient before showing the window, otherwise it won't work
+		self.realize()
+		gdkWindow = gtk.gdk.window_foreign_new(platformWindow.handle)
+		self.get_window().set_transient_for(gdkWindow)
+		self.show()
+
+	def handle_window_geometry_changed(self, platformWindow):
+		x, y = platformWindow.geometry[:2]
+		self.move(x, y)
+
+	def handle_window_destroyed(self, platformWindow):
+		self.destroy()
+
+
+class HudManager(gtk.Window):
+
+	def __init__(self):
+		gtk.Window.__init__(self)
+		self.set_title('HudManager')
+
+		self.huds = {}
+		self.windowManager = Platform.WindowManager()
+
+		gobject.timeout_add(500, self.on_window_manager_timeout)
+
+	def on_window_manager_timeout(self):
+		"""event dispatcher for our HUDs"""
+
+		for message, param in self.windowManager.next():
+
+			#TODO: something goes wrong when the lobby pops up with the "connecting.."
+			# dialog. seems like we get no message that the lobby has been created
+			if message == self.windowManager.EVENT_WINDOW_CREATED:
+				platformWindow = param
+				if platformWindow.application == 'PokerStars.exe' and platformWindow.title == 'PokerStars Lobby':
+					self.huds[platformWindow.handle] = Hud(platformWindow)
+
+			elif message == self.windowManager.EVENT_WINDOW_GEOMETRY_CHANGED:
+				platformWindow = param
+				hud = self.huds.get(platformWindow.handle, None)
+				if hud is not None:
+					hud.handle_window_geometry_changed(platformWindow)
+
+			elif message == self.windowManager.EVENT_WINDOW_DESTROYED:
+				platformWindow = param
+				hud = self.huds.get(platformWindow.handle, None)
+				if hud is not None:
+					hud.handle_window_destroyed(platformWindow)
+					del self.huds[platformWindow.handle]
+
+		return True
+
+#************************************************************************************
+#
+#************************************************************************************
+if __name__ == '__main__':
+	m = HudManager()
+	m.connect('destroy', gtk.main_quit)
+	m.show()
+	gtk.main()
