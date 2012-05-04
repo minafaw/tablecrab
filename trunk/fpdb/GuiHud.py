e16fcba1951206f484eba57a395420230e5c1aac
jUrner@arcor.de <jUrner@arcor.de@c2506444-9b37-11de-bff5-93fd3790126f>
2012-05-04 09:32:29 +0000

    #fpdb# HUD can now store sizes in HudManager and resize associated windows

--- trunk/fpdb/GuiHud.py
+++ trunk/fpdb/GuiHud.py
@@ -9,6 +9,12 @@ import gobject
 
 import WindowManager
 
+try:
+	import L10n
+	_ = L10n.get_translation()
+except ImportError:
+	_ = lambda text: text
+
 #************************************************************************************
 #
 #************************************************************************************
@@ -46,10 +52,14 @@ import WindowManager
 
 
 class Hud(gtk.Window):
-	def __init__(self, wmWindow):
+
+	def __init__(self, hudManager, wmWindow):
 		gtk.Window.__init__(self)
 
+		self.hudManager = hudManager
 		self.wmWindow = wmWindow
+		self.tableSizeIndex = -1
+
 		self.set_title('Hud')
 		self.set_decorated(0)
 		self.set_focus(None)
@@ -65,19 +75,25 @@ class Hud(gtk.Window):
 		gdkWindow = gtk.gdk.window_foreign_new(wmWindow.handle)
 		self.get_window().set_transient_for(gdkWindow)
 
-		self.button1 = gtk.Button('+')
-		self.button2 = gtk.Button('Resize')
-		self.button2.connect("clicked", self.on_button_resize_clicked)
+		self.buttonAddTableSize = gtk.Button('+')
+		self.buttonAddTableSize.connect("clicked", self.on_button_add_table_size_clicked)
+		self.buttonResizeTable = gtk.Button('Resize')
+		self.buttonResizeTable.connect("clicked", self.on_button_resize_table_clicked)
 
+		#
+		self.retranslate()
+		self.init_layout()
+
+	def init_layout(self):
 		box = gtk.HBox()
 		self.add(box)
-		box.pack_start(self.button1, expand=False)
-		box.pack_start(self.button2, expand=False)
-
+		box.pack_start(self.buttonAddTableSize, expand=False)
+		box.pack_start(self.buttonResizeTable, expand=False)
 		self.show_all()
-		#self.show()
 
-		print bool(gtk.gdk.window_foreign_new(1234567))
+	def retranslate(self):
+		self.buttonAddTableSize.set_tooltip_text(_('Add table size'))
+		self.buttonResizeTable.set_tooltip_text(_('Resize table to next size'))
 
 	def handle_window_geometry_changed(self, wmWindow):
 		self.wmWindow = wmWindow
@@ -87,9 +103,96 @@ class Hud(gtk.Window):
 		self.wmWindow = None
 		self.destroy()
 
-	def on_button_resize_clicked(self, button):
-		self.wmWindow.set_size(600, 300)
+	def on_button_add_table_size_clicked(self, button):
+		#TODO: we may have to update wmWindow here
+		self.hudManager.add_table_size(None, self.wmWindow.geometry.w, self.wmWindow.geometry.h)
 
+	def on_button_resize_table_clicked(self, button):
+		sizes = self.hudManager.get_table_sizes()
+		if sizes:
+			lastSize = sizes[self.tableSizeIndex]
+			self.tableSizeIndex = sizes.index(lastSize) +1
+			if self.tableSizeIndex >= len(sizes):
+				self.tableSizeIndex = 0
+			size = sizes[self.tableSizeIndex]
+			self.wmWindow.set_size(size[1], size[2])
+
+class TableSizesModel(gtk.ListStore):
+	"""(gtk.ListStore) directory list"""
+
+	#TODO: not shure if runtime translation works like this
+	COLUMNS = (
+			('name', str, _('Name')),
+			('size', str, _('Size')),
+			)
+	def __init__(self, treeView):
+		"""constructor
+		@param treeView: treeView to associate the list to
+		"""
+
+		self._treeView = treeView
+
+		# init ListStore
+		self._columns, columnTypes = {}, []
+		for i, (name, type_, _) in enumerate(self.COLUMNS):
+			self._columns[name] = i
+			columnTypes.append(type_)
+		gtk.ListStore.__init__(self, *columnTypes)
+
+		# init TreeView
+		for i, (_, _type, _) in enumerate(self.COLUMNS):
+			column = gtk.TreeViewColumn()
+			self._treeView.append_column(column)
+			cell = gtk.CellRendererText()
+			column.pack_start(cell, True)
+			column.add_attribute(cell, 'text', i)
+		self._treeView.set_model(self)
+
+	def append_row(self, **kws):
+		"""appends a row to the list
+		@param kws: columnName1=value, columnName2=value
+		"""
+		columns = []
+		#NOTE: we have to initialize all columns, otherwise gtk returns None as cell value
+		for i, (name, _, _) in enumerate(self.COLUMNS):
+			value = kws.pop(name, '')
+			columns.append(i)
+			columns.append(value)
+		self.set(self.append(), *columns)
+		if kws:
+			raise ValueError('no such column. %s' % kws.keys()[0])
+
+	def get_column_index(self, colName):
+		"""returns index af a column given its column name
+		@param colName: (str) name of the column
+		@return: (int)
+		"""
+		return self._columns[colName]
+
+	def get_row_count(self):
+		"""returns number of rows in the list
+		@return: (int)
+		"""
+		return self.iter_n_children(None)
+
+	def get_value(self, row, colName):
+		"""returns value of a column
+		@param row: row the column resides in
+		@param colName: (str) name of the column
+		"""
+		return self[row][self._columns[colName]]
+
+	def retranslate(self):
+		for i, column in enumerate(self._treeView.get_columns()):
+			column.set_title(_(self.COLUMNS[i][2]))
+
+	def set_value(self, row, colName, value):
+		"""sets the value of a column
+		@param row: row the column resides in
+		@param colName: (str) name of the column
+		@param value: value to set
+		"""
+		self[row][self._columns[colName]] = value
 
 
 
@@ -102,7 +205,33 @@ class HudManager(gtk.Window):
 		self.huds = {}
 		self.windowManager = WindowManager.WindowManager()
 
+		self.tableSizesList = gtk.TreeView()
+		self.tableSizesScroll = gtk.ScrolledWindow()
+		self.tableSizesScroll.add(self.tableSizesList)
+		self.tableSizesModel = TableSizesModel(self.tableSizesList)
+		self.tableSizes = []
+
+		#
+		self.init_layout()
+		self.retranslate()
 		gobject.timeout_add(500, self.on_window_manager_timeout)
+
+	def init_layout(self):
+		self.add(self.tableSizesScroll)
+		self.show_all()
+
+	def retranslate(self):
+		self.tableSizesModel.retranslate()
+
+	def get_table_sizes(self):
+		return self.tableSizes
+
+	def add_table_size(self, name, w, h):
+		size = '%sx%s' % (w, h)
+		if name is None:
+			name = ''
+		self.tableSizes.append((name, w, h))
+		self.tableSizesModel.append_row(name=name, size=size)
 
 	def on_window_manager_timeout(self):
 		"""event dispatcher for our HUDs"""
@@ -114,7 +243,8 @@ class HudManager(gtk.Window):
 			if event == self.windowManager.EVENT_WINDOW_CREATED:
 				wmWindow = param
 				if wmWindow.application == 'PokerStars.exe' and wmWindow.title == 'PokerStars Lobby':
-					self.huds[wmWindow.handle] = Hud(wmWindow)
+					hud = Hud(self, wmWindow)
+					self.huds[wmWindow.handle] = hud
 
 			elif event == self.windowManager.EVENT_WINDOW_GEOMETRY_CHANGED:
 				wmWindow = param
